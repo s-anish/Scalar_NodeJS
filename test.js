e3e383de8ed9268439ad51de8c7835233e9cf8d5
s-anish <anishflorida@gmail.com>
2023-09-04 15:12:43 +0530

    V1.0

---
+++ test.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+fs.writeFileSync('new.txt','hello')
+//fs.unlinkSync('new.txt')
+
+console.log(__dirname)
+console.log(__filename)
+
+// fs.mkdirSync
+// fs.rmdirSync
+// fs.existsSync - for file and dir
+// fs,fs.readdirSync
